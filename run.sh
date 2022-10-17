6aef6ab26371c7fe8d3da29203c0de1d8942429c
Andrew Ferrin <ferrinaj@vt.edu>
2022-10-17 08:57:19 -0400

    update scripts for Project 1

--- run.sh
+++ run.sh
@@ -9,23 +9,23 @@ fi
 
 # If we've already compiled
 if [[ -d compiled ]]; then
-    # and the HW3.class exists
-    if [[ -f compiled/HW3.class ]]; then
+    # and the PR1.class exists
+    if [[ -f compiled/PR1.class ]]; then
         # If we should read from stdin...
         if [ $FROM_STDIN -eq 1 ]; then
             # run as such
-            java -cp .:antlr-4.10.1-complete.jar:compiled HW3
+            java -cp .:antlr-4.10.1-complete.jar:compiled PR1
         else
             # Make sure the file provided exists
             if [[ -f $1 ]]; then
                 # run the ANTLER grammar on the given file
-                java -cp .:antlr-4.10.1-complete.jar:compiled HW3 < $1
+                java -cp .:antlr-4.10.1-complete.jar:compiled PR1 < $1
             else
                 echo "Cannot find file: $1"
             fi
         fi
     else
-        echo "Cannot find 'HW3'"
+        echo "Cannot find 'PR1'"
     fi
 else
     echo "Please run compile.sh before running."
