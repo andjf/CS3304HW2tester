d58b776e55a048bccafd68c72a81380645688657
Andrew <ferrinaj@vt.edu>
2022-10-25 22:35:04 -0400

    Update for PR1

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 
 # Make sure that the ANTLER jar file exists
 if [[ -f antlr-4.10.1-complete.jar ]]; then
-    # Ensure that the src directory exists and src/HW2eval.java exits
+    # Ensure that the src directory exists and src/PR1.java exits
     if [[ -d src && -f src/PR1.java ]]; then
         # Compile the java java code into the compiled directory
         javac -d ./compiled -cp ./antlr-4.10.1-complete.jar src/*.java
